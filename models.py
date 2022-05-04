1899e1ebb887ac2dfefa7973aa4f901c9ef15474
eunjeong <ungdunge2@gmail.com>
2022-05-03 17:19:30 -0700

    Make Course database table

--- models.py
+++ models.py
@@ -11,3 +11,14 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Course(db.Model):
+    __tablename__ = "course"
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.String(9), unique=False, nullable=False)
+    class_name = db.Column(db.String(30), unique=False, nullable=False)
+    class_credit = db.Column(db.Float, unique=False, nullable=False)
+    grade = db.Column(db.Integer, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Grade: {self.grade}'
